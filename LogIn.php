5fcc82c525a58b8e9860e22672d2a20c7718401a
kofia894 <kofia894@gmail.com>
2021-02-26 19:00:27 +0000

    organized

--- LogIn.php
+++ LogIn.php
@@ -8,19 +8,18 @@
       <!-- Bootstrap CSS -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-      <title>Sign Up</title>
-      <link href="css/style.css" type="text/css" rel="stylesheet">
-     
+      <title>Log In</title>
+      <link href="assets/style.css" type="text/css" rel="stylesheet">
   </head>
-  <body class = "container-fluid mt-5" style="background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(expenseimage.jpeg); height: 100vh; background-size: cover; background-position: center;" >
+  <body class = "container-fluid mt-5"  style="background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(assets/img/expenseimage.jpeg); height: 100vh; background-size: cover; background-position: center;">
 
-    <div class="justify-content-center d-flex w-100 p-3 " >
+    <div class=" container-fluid justify-content-center d-flex w-100 p-3 ">
         
-        <form class=" text-center bg-white mt-5 border border-light p-5 shadow-lg rounded" style="width: 35%;" action="#" method="POST" id="Sign-up-form">
+        <form class=" text-center bg-white mt-5 border border-light p-5 shadow-lg rounded" style="width: 35%;" action="#" method="POST" id="Sign-in-form">
             
            
             
-            <p class="h4 mb-4">Sign Up</p>
+            <p class="h4 mb-4">Log In</p>
             <p class="mb-4"><em></em></p>
 
             
@@ -32,13 +31,6 @@
                   </div>
               </div>
 
-              <div class="row no-gutters"><div id="error-block-email" class="error-block-message col-md-8 offset-md-4 text-left"></div></div>
-              <div class="form-group row no-gutters mb-4">
-                  <label class="col-md-4 col-form-label text-left" for="email">Email address:</label>
-                  <div class="col-md-8">
-                      <input type="email" id="email" name="email" class="form-control">
-                  </div>
-              </div>
 
             
            
@@ -53,13 +45,7 @@
                 </div>
             </div>
 
-            <div class="row no-gutters"><div id="error-block-repassword" class="error-block-message col-md-8 offset-md-4 text-left"></div></div>
-            <div class="form-group row no-gutters">
-                <label class="col-md-4 col-form-label text-left" for="spassword2">Reenter password:</label>
-                <div class="col-md-8">
-                    <input type="password" id="pword2" name="pword2" class="form-control">
-                </div>
-            </div>
+           
 
             
             <!-- <div class="d-flex justify-content-end mb-4">
@@ -69,12 +55,11 @@
          
             <!-- </div> --> 
 
-            <div class="d-flex mb-4 ml-2">
-               
-              <button class="btn btn-success btn-block btn-info my-4 mx-auto" style="width: 150px;" name="login" type="submit">Sign in</button>
+            <div class="d-flex justify-content-end mb-4">
+              <button class="btn btn-success btn-block btn-info my-4 mx-auto "  name="login" type="submit">Log in</button>
           </div>
             
-            <p>Already have a account ? Click <a class="a" href="LogIn.html">here</a> to sign in now!</p>
+            <p>New here ? Click <a class="a" href="SignUp.html">here</a> to sign in now!</p>
             
         </form>
         
@@ -86,6 +71,6 @@
       <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-      <script src = "js/signup_validate.js"></script>
+      <script src = "assets/login_validate.js"></script>
 </body>
 </html>
--- SignUp.php
+++ SignUp.php
@@ -8,18 +8,19 @@
       <!-- Bootstrap CSS -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-      <title>Log In</title>
-      <link href="css/style.css" type="text/css" rel="stylesheet">
+      <title>Sign Up</title>
+      <link href="assets/style.css" type="text/css" rel="stylesheet">
+     
   </head>
-  <body class = "container-fluid mt-5"  style="background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(expenseimage.jpeg); height: 100vh; background-size: cover; background-position: center;">
+  <body class = "container-fluid mt-5" style="background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(assets/img/expenseimage.jpeg); height: 100vh; background-size: cover; background-position: center;" >
 
-    <div class=" container-fluid justify-content-center d-flex w-100 p-3 ">
+    <div class="justify-content-center d-flex w-100 p-3 " >
         
-        <form class=" text-center bg-white mt-5 border border-light p-5 shadow-lg rounded" style="width: 35%;" action="#" method="POST" id="Sign-in-form">
+        <form class=" text-center bg-white mt-5 border border-light p-5 shadow-lg rounded" style="width: 35%;" action="#" method="POST" id="Sign-up-form">
             
            
             
-            <p class="h4 mb-4">Log In</p>
+            <p class="h4 mb-4">Sign Up</p>
             <p class="mb-4"><em></em></p>
 
             
@@ -31,6 +32,13 @@
                   </div>
               </div>
 
+              <div class="row no-gutters"><div id="error-block-email" class="error-block-message col-md-8 offset-md-4 text-left"></div></div>
+              <div class="form-group row no-gutters mb-4">
+                  <label class="col-md-4 col-form-label text-left" for="email">Email address:</label>
+                  <div class="col-md-8">
+                      <input type="email" id="email" name="email" class="form-control">
+                  </div>
+              </div>
 
             
            
@@ -45,7 +53,13 @@
                 </div>
             </div>
 
-           
+            <div class="row no-gutters"><div id="error-block-repassword" class="error-block-message col-md-8 offset-md-4 text-left"></div></div>
+            <div class="form-group row no-gutters">
+                <label class="col-md-4 col-form-label text-left" for="spassword2">Reenter password:</label>
+                <div class="col-md-8">
+                    <input type="password" id="pword2" name="pword2" class="form-control">
+                </div>
+            </div>
 
             
             <!-- <div class="d-flex justify-content-end mb-4">
@@ -55,11 +69,12 @@
          
             <!-- </div> --> 
 
-            <div class="d-flex justify-content-end mb-4">
-              <button class="btn btn-success btn-block btn-info my-4 mx-auto "  name="login" type="submit">Log in</button>
+            <div class="d-flex mb-4 ml-2">
+               
+              <button class="btn btn-success btn-block btn-info my-4 mx-auto" style="width: 150px;" name="login" type="submit">Sign in</button>
           </div>
             
-            <p>New here ? Click <a class="a" href="SignUp.html">here</a> to sign in now!</p>
+            <p>Already have a account ? Click <a class="a" href="LogIn.html">here</a> to sign in now!</p>
             
         </form>
         
@@ -71,6 +86,6 @@
       <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-      <script src = "js/login_validate.js"></script>
+      <script src = "assets/signup_validate.js"></script>
 </body>
 </html>
